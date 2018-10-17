10ac1c035f0dc28bf19cb1019b9fc3f2a858f216
haolun zhang <opensource@infra.top>
2018-10-17 16:20:43 +0800

    fix variable IMAGE_PREFIX

--- build.sh
+++ build.sh
@@ -9,18 +9,18 @@ WORK_DIR=$(pwd)
 
 if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
-export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
+export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo/};
 export IMAGE_NAME=${IMAGE_NAME:-docker}
 export IMAGE_TAG=$(echo ${IMAGE_ARG_DOCKERCE_VERSION:-18.06.1~ce~3-0} | awk -F'~' '{print $1}')-bionic
 if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
 
 # Build image
-if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q ${IMAGE_PREFIX}${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
     docker-compose build image
 fi
 
 # Build dumper image
-docker save ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} > dumper/docker/image.tar
+docker save ${IMAGE_PREFIX}${IMAGE_NAME}:${IMAGE_TAG} > dumper/docker/image.tar
 docker-compose build dumper
 
 # Build archive image
